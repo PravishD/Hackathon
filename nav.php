866f3eb52dc7471fdc25823b7b37f619a426e5fb
croan0406 <khushan.chumun@spoonconsulting.com>
2023-11-08 21:52:12 +0400

    21:42

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <div id="nav">
   <ul>
-     <li><a href="index.html">Phishing</a></li>
+     <li><a href="phishing.php">Phishing</a></li>
      <li><a href="about.html">Identity Theft</a></li>
      <li><a href="products.html">Online Shopping</a></li>
      <li><a href="support.html">Phone Frauds</a></li>
--- phishing.php
+++ phishing.php
@@ -1,33 +1,54 @@
 <!DOCTYPE html>
 <html>
-<head>
- <title>Basic CSS Layout</title>
- <meta charset="utf-8"/>
- <link rel="stylesheet" href="style.css" type="text/css"/>
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
+  <head>
+    <title>Basic CSS Layout</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+  </head>
+
+  
+
+  <body>
+
+    <header class="header">
+      
+    </header>
+
+    <?php include 'nav.php';?>
+
+
+    <div id="section">
+      <h2>Phishing</h2>
+
+      <div class="ratio ratio-16x9" id="video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/kDFeSUUwRnA?si=i3kvDXd-mQCqQh1V" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <p>Phishing is like a digital form of fishing, where cybercriminals use tricks to try and catch your personal and sensitive information, just like a fisherman tries to catch fish. Here's a simple explanation for people of all ages:
+
+      Imagine you get an email, text message, or even a website that looks like it's from a trustworthy source, like your bank, a friend, or a popular online service. It might ask you to click on a link, enter your username and password, or provide your credit card details. However, this message is a fake, and the people behind it are actually trying to steal your personal information.
+      </p>
+
+    <div id="buttons">
+      <a href="destination-page.html">
+        <button type="button" class="button">Take Quiz</button>  
+      </a>
+      <a href="destination-page.html">
+        <button type="button" class="button" >Next</button> 
+      </a>
+        
+        
+    </div>
+    
+    
+    </div>
+
+    
  
-<div id="header">
-
-</div>
-
-<?php include 'nav.php';?>
 
 
-<div id="section">
- <h2>Phishing</h2>
- <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
- <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
-</div>
-
-<div class="ratio ratio-16x9">
-  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
-</div>
-
- 
-<div id="footer">
- &copy; 2023.
-</div>
-</body>
+    
+    <div id="footer">
+      &copy; 2023.
+    </div>
+  </body>
 </html>
