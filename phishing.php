2327a0e73bf32182c7b22c68ec6827152e031969
croan0406 <khushan.chumun@spoonconsulting.com>
2023-11-08 15:05:05 +0400

    Initial

---
+++ phishing.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Basic CSS Layout</title>
+ <meta charset="utf-8"/>
+ <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+ 
+<div id="header">
+ <h1>Online Fraud Sensitisation</h1>
+</div>
+ 
+<div id="nav">
+  <ul>
+     <li><a href="index.html">Phishing</a></li>
+     <li><a href="about.html">Identity Theft</a></li>
+     <li><a href="products.html">Online Shopping</a></li>
+     <li><a href="support.html">Phone Frauds</a></li>
+     <li><a href="contact.html">Popup ads</a></li>
+  </ul>
+</div>
+ 
+<div id="section">
+ <h2>Phishing</h2>
+ <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
+ <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
+</div>
+ 
+<div id="footer">
+ &copy; 2023.
+</div>
+</body>
+</html>
