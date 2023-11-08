04dc2a44d8477533ff91f58968f1009ae00b1a01
croan0406 <khushan.chumun@spoonconsulting.com>
2023-11-08 16:06:09 +0400

    2nd

---
+++ nav.php
@@ -0,0 +1,9 @@
+<div id="nav">
+  <ul>
+     <li><a href="index.html">Phishing</a></li>
+     <li><a href="about.html">Identity Theft</a></li>
+     <li><a href="products.html">Online Shopping</a></li>
+     <li><a href="support.html">Phone Frauds</a></li>
+     <li><a href="contact.html">Popup ads</a></li>
+  </ul>
+</div>
--- phishing.php
+++ phishing.php
@@ -4,28 +4,27 @@
  <title>Basic CSS Layout</title>
  <meta charset="utf-8"/>
  <link rel="stylesheet" href="style.css" type="text/css"/>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
  
 <div id="header">
- <h1>Online Fraud Sensitisation</h1>
+
 </div>
- 
-<div id="nav">
-  <ul>
-     <li><a href="index.html">Phishing</a></li>
-     <li><a href="about.html">Identity Theft</a></li>
-     <li><a href="products.html">Online Shopping</a></li>
-     <li><a href="support.html">Phone Frauds</a></li>
-     <li><a href="contact.html">Popup ads</a></li>
-  </ul>
-</div>
- 
+
+<?php include 'nav.php';?>
+
+
 <div id="section">
  <h2>Phishing</h2>
  <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
  <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.</p>
 </div>
+
+<div class="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+</div>
+
  
 <div id="footer">
  &copy; 2023.
