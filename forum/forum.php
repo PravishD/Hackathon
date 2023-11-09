6fb9d4dc3a8fb22e22c8e1f2782b6ee8a4c35ffe
ruslan-11 <ruslan.gogoah@spoonconsulting.com>
2023-11-09 17:04:57 +0400

    forum styles

--- forum/forum.php
+++ forum/forum.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Forum</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
         $(document).ready(
@@ -102,7 +104,8 @@
 
 <body>
     <header>
-        <h1>Simple Forum</h1>
+        <h1>Forum</h1>
+        <?php include('') ?>
     </header>
 
     <main>
@@ -123,15 +126,20 @@
             ?>
             <!-- Add more comments as needed -->
         </div>
-
-        <form method='post' action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-            <label for="comment">Your Comment:</label>
-            <textarea id="comment" name="txt_comment" rows="4" required></textarea>
-            <button type="submit">Submit Comment</button>
-        </form>
+        
+        <div class="form-floating">
+            <form method='post' action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                <label for="comment">Your Comment:</label>
+                <textarea id="comment" placeholder="Leave a comment" name="txt_comment" rows="4" required></textarea>
+                <button class="btn-primary" type="submit">Submit Comment</button>
+            </form>
+        </div>
+        
     </main>
 
 
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </html>
